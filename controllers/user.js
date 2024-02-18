edc135378a9b19522f72e7d901c4108f9c40d2c0
Rohit Londhe <96713583+rohitlondhe265@users.noreply.github.com>
2024-02-18 09:46:40 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,22 @@
-import Result from '../database/models/result.js';
-import User from '../database/models/user.js';
-import dbConnect from '../database/dbConnect.js';
+import Result from "../database/models/result.js";
+import User from "../database/models/user.js";
+import dbConnect from "../database/dbConnect.js";
 
 export async function addUser(req, res) {
+  const { email } = req.body;
   try {
     await dbConnect();
-    const newUser = new User(req.body);
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    // Check if the email exists in the database
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      // If the email exists, send a message
+      return res.status(200).json({ message: "Email exists in the database" });
+    } else {
+      // If the email does not exist, create a new user
+      await User.create(req.body);
+      return res.status(201).json({ message: "User created successfully" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -28,7 +37,7 @@ export async function getUser(req, res) {
     await dbConnect();
     const user = await User.findById(req.params.id);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
     res.json(user);
   } catch (error) {
@@ -43,7 +52,7 @@ export async function updateUser(req, res) {
       new: true,
     });
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
     res.json(user);
   } catch (error) {
@@ -55,9 +64,9 @@ export async function deleteUser(req, res) {
   const userId = req.params.id;
   try {
     await dbConnect();
-    await User.findByIdAndDelete(userId)
+    await User.findByIdAndDelete(userId);
     await Result.deleteMany({ userId });
-    res.json({ message: 'User and associated results deleted' });
+    res.json({ message: "User and associated results deleted" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
