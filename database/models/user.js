c77de7f5a4195549109a03936e0ffaef2cfd43ce
Rohit Londhe <96713583+rohitlondhe265@users.noreply.github.com>
2024-02-18 09:47:07 +0530

    Update user.js

--- database/models/user.js
+++ database/models/user.js
@@ -3,17 +3,18 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
     required: true,
   },
   userName: String,
   subscriptions: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Category',
+      ref: "Category",
     },
   ],
 });
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
